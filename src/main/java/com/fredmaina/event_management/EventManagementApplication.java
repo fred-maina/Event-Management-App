2afdc9da1faba9246f44a14fb6bfadf34f73d853
Fredrick Maina <fchege04@gmail.com>
2024-11-29 23:29:15 +0300

    Create Main apps and Test

---
+++ src/main/java/com/fredmaina/event_management/EventManagementApplication.java
@@ -0,0 +1,15 @@
+package com.fredmaina.event_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude = {SecurityAutoConfiguration.class})
+public class EventManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventManagementApplication.class, args);
+	}
+}
