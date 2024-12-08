f4b9597cf32957d992d47fcbc7f0955f7afd5cdd
Fredrick Maina <fchege04@gmail.com>
2024-12-08 20:52:51 +0300

    Return uniform API messages

--- src/main/java/com/fredmaina/event_management/controllers/EventController.java
+++ src/main/java/com/fredmaina/event_management/controllers/EventController.java
@@ -1,10 +1,12 @@
 package com.fredmaina.event_management.controllers;
 
+import com.fredmaina.event_management.DTOs.APIResponse;
 import com.fredmaina.event_management.DTOs.EventDto;
 import com.fredmaina.event_management.models.Event;
 import com.fredmaina.event_management.services.EventService;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -19,13 +21,18 @@ public class EventController {
     EventService eventService;
 
     @PostMapping("/create")
-    public ResponseEntity<Event> createEvent(@RequestBody EventDto eventDto){
+    public ResponseEntity<APIResponse<Event>> createEvent(@RequestBody EventDto eventDto){
         System.out.println(eventDto.getCreatorId());
         Optional<Event> eventOptional = eventService.createEvent(eventDto);
-        if (eventOptional.isEmpty()){
-            return ResponseEntity.notFound().build();
+        if (eventOptional.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                    new APIResponse<>(false, "Event creation failed. Please check your input.", null)
+            );
         }
-        return ResponseEntity.ok(eventOptional.get());
+
+        return ResponseEntity.ok(
+                new APIResponse<Event>(true,"Event created successfully",eventOptional.get())
+        );
     }
     @GetMapping("/get/{creator_id}")
     public ResponseEntity<List<Event>> getEventByCreator(@PathVariable int creator_id){
@@ -34,6 +41,11 @@ public class EventController {
     @GetMapping("/get/")
     public ResponseEntity<List<Event>> getAllEvents(){
         return  ResponseEntity.of(eventService.getAllEvents());
+    }
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteEvent(@PathVariable int id){
+        eventService.deleteEventById(id);
+        return ResponseEntity.ok("Event deleted successfully");
     }
 
 
