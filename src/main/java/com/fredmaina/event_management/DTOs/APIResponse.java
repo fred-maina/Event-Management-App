866d3341cca2a4d03f09ac2be7ee89c2071ac3a8
Fredrick Maina <fchege04@gmail.com>
2024-12-08 20:52:30 +0300

    Create an API Response Class to make API Responses uniform.

---
+++ src/main/java/com/fredmaina/event_management/DTOs/APIResponse.java
@@ -0,0 +1,20 @@
+package com.fredmaina.event_management.DTOs;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.apache.logging.log4j.message.Message;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class APIResponse<T> {
+    private Boolean success;
+    private String message;
+    private T data;
+    APIResponse(Boolean success,String message){
+        this.success=success;
+        this.message=message;
+    }
+
+}
