7f2e227fbc2228f269fa3728323239ad75ed98b3
Fredrick Maina <fchege04@gmail.com>
2024-12-08 12:50:02 +0300

    Create the Entity that maps to the event

---
+++ src/main/java/com/fredmaina/event_management/models/Event.java
@@ -0,0 +1,41 @@
+package com.fredmaina.event_management.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name="Events")
+@AllArgsConstructor
+@ToString
+@NoArgsConstructor
+public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="event_id")
+    private Integer id;
+
+    @Column(name="event_name",nullable = false,length = 255)
+    private  String eventName;
+
+    @Column(name="event_start_date",nullable = false)
+    private LocalDateTime eventStartDate;
+
+    @Column(name="event_end_date")
+    private LocalDateTime eventEndDate;
+
+    @Column(name="event_venue",nullable = false,length = 255)
+    private String eventVenue;
+
+    @Column(name="event_capacity",nullable = false)
+    private int eventCapacity=-1;//capacity -1 means the space is unlimited
+
+    @JoinColumn(name="creator_id",referencedColumnName = "user_id")
+    @ManyToOne(fetch = FetchType.EAGER)
+    private User creator;
+
+
+}
