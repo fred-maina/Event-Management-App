c0fd82047cc368e04fbec30e65d06f390f60beca
Fredrick Maina <fchege04@gmail.com>
2024-12-08 12:53:14 +0300

    Create a security config file

---
+++ src/main/java/com/fredmaina/event_management/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.fredmaina.event_management.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable) // Updated way to disable CSRF
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll()); // Allow all requests
+        return http.build();
+    }
+}
