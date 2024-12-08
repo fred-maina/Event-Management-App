60d8744e6fb2ccc984b466dd20172a880c5a4085
Fredrick Maina <fchege04@gmail.com>
2024-12-08 12:49:45 +0300

    Hash the password before saving in database

--- src/main/java/com/fredmaina/event_management/services/AuthService.java
+++ src/main/java/com/fredmaina/event_management/services/AuthService.java
@@ -3,15 +3,28 @@ package com.fredmaina.event_management.services;
 import com.fredmaina.event_management.DTOs.RegisterRequest;
 import com.fredmaina.event_management.models.User;
 import com.fredmaina.event_management.repositories.UserRepository;
+import com.fredmaina.event_management.utils.PasswordUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class AuthService {
     @Autowired
     UserRepository userRepository;
-    public void registerUser(RegisterRequest registerRequest){
-        User user = new User(registerRequest.getFirstName(),registerRequest.getLastName(),registerRequest.getEmail(),registerRequest.getPassword());
-        userRepository.save(user);
+    public Optional<User> registerUser(RegisterRequest registerRequest){
+        String hashedPassword = PasswordUtil.encodePassword(registerRequest.getPassword());
+        User user = new User(registerRequest.getFirstName(),registerRequest.getLastName(),registerRequest.getEmail(),hashedPassword);
+        try {
+            userRepository.save(user);
+            return Optional.of(user);
+        }
+        catch (Exception e){
+            System.err.println("Error Registering the user: "+e.getMessage());
+            return Optional.empty();
+
+        }
+
     }
 }
