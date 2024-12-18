d84db0298690a9d7dd02c9384b17953ea09d4836
Fredrick Maina <fchege04@gmail.com>
2024-12-18 23:52:30 +0300

    Add number of tickets per ticketype

--- src/main/java/com/fredmaina/event_management/DTOs/TicketTypeDTO.java
+++ src/main/java/com/fredmaina/event_management/DTOs/TicketTypeDTO.java
@@ -1,8 +1,19 @@
 package com.fredmaina.event_management.DTOs;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class TicketTypeDTO {
     int id;
     String typeCategory;
+    int numberOfTickets;
     int price;
     int eventId;
 }
