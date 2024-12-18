2e798f6891b55cf9211a60332eb0a159911af0e3
Fredrick Maina <fchege04@gmail.com>
2024-12-18 23:53:15 +0300

    Create a TicketTypeService

---
+++ src/main/java/com/fredmaina/event_management/services/TicketTypeService.java
@@ -0,0 +1,20 @@
+package com.fredmaina.event_management.services;
+
+
+import com.fredmaina.event_management.models.TicketType;
+import com.fredmaina.event_management.repositories.TicketTypeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TicketTypeService {
+    @Autowired
+    TicketTypeRepository ticketTypeRepository;
+    public void createTicketType(List<TicketType> ticketTypes){
+        ticketTypeRepository.saveAll(ticketTypes);
+    }
+
+
+}
