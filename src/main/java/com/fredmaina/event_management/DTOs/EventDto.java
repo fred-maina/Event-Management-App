77881dbe869d92c027a34a88706b76ba8d460d09
Fredrick Maina <fchege04@gmail.com>
2024-12-19 17:58:53 +0300

    Add a Noargs constructor

--- src/main/java/com/fredmaina/event_management/DTOs/EventDto.java
+++ src/main/java/com/fredmaina/event_management/DTOs/EventDto.java
@@ -1,10 +1,7 @@
 package com.fredmaina.event_management.DTOs;
 
 import com.fredmaina.event_management.models.TicketType;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -13,6 +10,7 @@ import java.util.List;
 @Setter
 @AllArgsConstructor
 @ToString
+@NoArgsConstructor
 public class EventDto {
 
     private Integer id;
