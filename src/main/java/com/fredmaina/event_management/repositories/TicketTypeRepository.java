3f63075ffe8adb49ac2459d4bf2164f7a0e1755f
Fredrick Maina <fchege04@gmail.com>
2024-12-21 19:45:44 +0300

    Add the method for finding ticket types by event

--- src/main/java/com/fredmaina/event_management/repositories/TicketTypeRepository.java
+++ src/main/java/com/fredmaina/event_management/repositories/TicketTypeRepository.java
@@ -1,9 +1,11 @@
 package com.fredmaina.event_management.repositories;
 
+import com.fredmaina.event_management.models.Event;
 import com.fredmaina.event_management.models.TicketType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface TicketTypeRepository extends JpaRepository<TicketType,Integer> {
+    List<TicketType> findAllByEvent(Event event);
 }
