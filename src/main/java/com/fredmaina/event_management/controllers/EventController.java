babfbeeb86653f82e7a58a41e5d041d77df96a83
Fredrick Maina <fchege04@gmail.com>
2024-12-24 17:07:06 +0300

    Change Event id to UUID

--- src/main/java/com/fredmaina/event_management/controllers/EventController.java
+++ src/main/java/com/fredmaina/event_management/controllers/EventController.java
@@ -1,5 +1,7 @@
 package com.fredmaina.event_management.controllers;
 
+import jakarta.validation.Valid;
+import org.springframework.http.MediaType;
 import com.fredmaina.event_management.DTOs.APIResponse;
 import com.fredmaina.event_management.DTOs.EventDto;
 import com.fredmaina.event_management.DTOs.TicketTypeDTO;
@@ -8,6 +10,7 @@ import com.fredmaina.event_management.models.User;
 import com.fredmaina.event_management.repositories.EventRepository;
 import com.fredmaina.event_management.repositories.UserRepository;
 import com.fredmaina.event_management.services.EventService;
+import com.fredmaina.event_management.services.S3Service;
 import com.fredmaina.event_management.services.TicketTypeService;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,9 +19,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.config.Task;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("api/events")
@@ -30,10 +37,21 @@ public class EventController {
     private UserRepository userRepository;
     @Autowired
     private TicketTypeService ticketTypeService;
+    @Autowired
+    private S3Service s3Service;
 
-    @PostMapping("/create")
-    public ResponseEntity<APIResponse<Event>> createEvent(@RequestBody EventDto eventDto){
+    @PostMapping(value="/create",consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<APIResponse<Event>> createEvent(@RequestPart("event") @Valid EventDto eventDto, @RequestPart("poster") MultipartFile poster){
+        System.out.println("Received EventDto: " + eventDto);
+        // Replace with your bucket name
+        String bucketName = "fredeventsystem";
 
+        // Generate a unique key for the file (use UUID or some unique string)
+        String key = UUID.randomUUID()+"_" + poster.getOriginalFilename(); // Or use a UUID to generate a unique filename
+
+        s3Service.uploadFileToS3(bucketName, poster, key);
+        String fileUrl = s3Service.getFileUrl(bucketName, key);
+        eventDto.setPosterUrl(fileUrl);
 
         Optional<User> userOptional = userRepository.findById(eventDto.getCreatorId());
         if (userOptional.isEmpty()) {
@@ -50,7 +68,7 @@ public class EventController {
 
     }
     @GetMapping("/get/{creator_id}")
-    public ResponseEntity<APIResponse<List<EventDto>>> getEventsByCreator(@PathVariable int creator_id){
+    public ResponseEntity<APIResponse<List<EventDto>>> getEventsByCreator(@PathVariable UUID creator_id){
         Optional<User> optionalUser = userRepository.findById(creator_id);
         if (optionalUser.isEmpty()){
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
@@ -73,6 +91,7 @@ public class EventController {
                 event.getEventVenue(),
                 event.getEventCapacity(),
                 event.getCreator().getId(),
+                event.getPosterUrl(),
                 ticketTypeService.findAllTicketTypesByEvent(event).stream().map(ticketType->{
             return new TicketTypeDTO(
                     ticketType.getId(),
@@ -108,7 +127,7 @@ public class EventController {
     }
 
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity<APIResponse<Event>> deleteEvent(@PathVariable int id){
+    public ResponseEntity<APIResponse<Event>> deleteEvent(@PathVariable UUID id){
         Optional<Event> optionalEvent = eventService.getEventById(id);
         if (optionalEvent.isEmpty()){
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
@@ -122,5 +141,21 @@ public class EventController {
         );
     }
 
+    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<Map<String, String>> uploadFileToS3(
+            @RequestParam("file") MultipartFile file) {
+
+        // Replace with your bucket name
+        String bucketName = "fredeventsystem";
+
+        // Generate a unique key for the file (use UUID or some unique string)
+        String key = file.getOriginalFilename(); // Or use a UUID to generate a unique filename
+
+        // Upload the file and get the URL
+        s3Service.uploadFileToS3(bucketName, file, key);
+        String fileUrl = s3Service.getFileUrl(bucketName, key);
+
+        return ResponseEntity.ok(Map.of("url", fileUrl));
+    }
 
 }
