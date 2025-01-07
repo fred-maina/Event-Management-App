0720b19931df364f463bb3e80c5c2fb85ca4acf7
Fredrick Maina <fchege04@gmail.com>
2025-01-07 21:44:30 +0300

    Add update method for updating ticket Types

--- src/main/java/com/fredmaina/event_management/services/TicketTypeService.java
+++ src/main/java/com/fredmaina/event_management/services/TicketTypeService.java
@@ -1,13 +1,17 @@
 package com.fredmaina.event_management.services;
 
 
+import com.fredmaina.event_management.DTOs.TicketTypeDTO;
 import com.fredmaina.event_management.models.Event;
 import com.fredmaina.event_management.models.TicketType;
 import com.fredmaina.event_management.repositories.TicketTypeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class TicketTypeService {
@@ -18,6 +22,23 @@ public class TicketTypeService {
     }
     public List<TicketType>  findAllTicketTypesByEvent(Event event){
         return ticketTypeRepository.findAllByEvent(event);
+    }
+    public Optional<TicketType> updateTicketType(UUID id, TicketTypeDTO ticketTypeDTO){
+        Optional<TicketType> ticketType=ticketTypeRepository.findById(id);
+        if (ticketType.isEmpty()){
+            return Optional.empty();
+        }
+        if (ticketTypeDTO.getNumberOfTickets() != 0){
+            ticketType.get().setNumberOfTickets(ticketTypeDTO.getNumberOfTickets());
+        }
+        if (ticketTypeDTO.getTypeCategory() != null){
+            ticketType.get().setTypeCategory(ticketTypeDTO.getTypeCategory());
+        }
+        if (ticketTypeDTO.getPrice() !=0){
+            ticketType.get().setPrice(ticketTypeDTO.getPrice());
+        }
+         return Optional.of(ticketTypeRepository.save(ticketType.get()));
+
     }
 
 
