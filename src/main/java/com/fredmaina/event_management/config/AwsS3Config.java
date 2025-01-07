e1abfcffad7ddd37c1abfa1dce929ac08f0b3864
Fredrick Maina <fchege04@gmail.com>
2025-01-07 21:40:47 +0300

    Move to AWS SDK v2

--- src/main/java/com/fredmaina/event_management/config/AwsS3Config.java
+++ src/main/java/com/fredmaina/event_management/config/AwsS3Config.java
@@ -1,10 +1,8 @@
 package com.fredmaina.event_management.config;
 
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.services.s3.S3Client;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,19 +10,19 @@ import org.springframework.context.annotation.Configuration;
 public class AwsS3Config {
 
     @Bean
-    public AmazonS3 amazonS3() {
-        // Hardcoded AWS credentials and region
+    public S3Client s3Client() {
+        // Retrieve AWS credentials and region from environment variables
         String awsRegion = System.getenv("AWS_REGION");                     // Replace with your desired region
-        String awsAccessKey =System.getenv("AWS_ACCESS_KEY_ID") ;     // Replace with your access key
-        String awsSecretKey = System.getenv("AWS_SECRET_ACCESS_KEY");  // Replace with your secret key
+        String awsAccessKey = System.getenv("AWS_ACCESS_KEY_ID");          // Replace with your access key
+        String awsSecretKey = System.getenv("AWS_SECRET_ACCESS_KEY");      // Replace with your secret key
 
+        AwsCredentialsProvider awsCredentialsProvider = StaticCredentialsProvider.create(
+                software.amazon.awssdk.auth.credentials.AwsBasicCredentials.create(awsAccessKey, awsSecretKey)
+        );
 
-        AWSCredentials awsCredentials = new BasicAWSCredentials(awsAccessKey, awsSecretKey);
-
-        return AmazonS3ClientBuilder
-                .standard()
-                .withRegion(awsRegion)
-                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+        return S3Client.builder()
+                .region(software.amazon.awssdk.regions.Region.of(awsRegion))
+                .credentialsProvider(awsCredentialsProvider)
                 .build();
     }
 }
