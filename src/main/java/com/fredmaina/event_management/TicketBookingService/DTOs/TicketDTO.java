e2365420a95ed337f14c2a0cc70ae56f3263eea7
Fredrick Maina <fchege04@gmail.com>
2025-01-16 17:37:55 +0300

    Create Ticket DTO class

---
+++ src/main/java/com/fredmaina/event_management/TicketBookingService/DTOs/TicketDTO.java
@@ -0,0 +1,26 @@
+package com.fredmaina.event_management.TicketBookingService.DTOs;
+
+import com.fredmaina.event_management.AuthService.models.User;
+import com.fredmaina.event_management.EventCreationService.Models.Event;
+import com.fredmaina.event_management.EventCreationService.Models.TicketType;
+import com.fredmaina.event_management.TicketBookingService.Models.Ticket;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+import lombok.EqualsAndHashCode;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class TicketDTO  {
+    private UUID ticketType;
+    private UUID event;
+}
