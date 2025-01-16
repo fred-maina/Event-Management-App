1cb95b62d0e883bceb880bca5a80661878991af3
Fredrick Maina <fchege04@gmail.com>
2025-01-16 17:38:05 +0300

    Create Ticket Repository class

---
+++ src/main/java/com/fredmaina/event_management/TicketBookingService/Repositories/TicketRepository.java
@@ -0,0 +1,11 @@
+package com.fredmaina.event_management.TicketBookingService.Repositories;
+
+import com.fredmaina.event_management.TicketBookingService.Models.Ticket;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface TicketRepository extends JpaRepository<Ticket, UUID> {
+    Ticket findTicketByTicketCode(String ticketCode);
+}
