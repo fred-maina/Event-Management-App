13af83cca04ecf6b00d23f4f914e56ced46b0e62
Fredrick Maina <fchege04@gmail.com>
2025-01-16 18:14:59 +0300

    Make the constructor public

--- src/main/java/com/fredmaina/event_management/globalservices/DTOs/APIResponse.java
+++ src/main/java/com/fredmaina/event_management/globalservices/DTOs/APIResponse.java
@@ -12,7 +12,7 @@ public class APIResponse<T> {
     private boolean success;
     private String message;
     private T data;
-    APIResponse(Boolean success,String message){
+    public APIResponse(Boolean success, String message){
         this.success=success;
         this.message=message;
     }
