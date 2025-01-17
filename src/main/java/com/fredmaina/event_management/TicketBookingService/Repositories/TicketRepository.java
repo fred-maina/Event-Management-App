2bc6faa1a62ee7945bc42bb54ede06c813ce12f2
Fredrick Maina <fchege04@gmail.com>
2025-01-17 22:29:56 +0300

    Find Ticket by ticket code now returns an optional

--- src/main/java/com/fredmaina/event_management/TicketBookingService/Repositories/TicketRepository.java
+++ src/main/java/com/fredmaina/event_management/TicketBookingService/Repositories/TicketRepository.java
@@ -4,8 +4,9 @@ import com.fredmaina.event_management.TicketBookingService.Models.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface TicketRepository extends JpaRepository<Ticket, UUID> {
-    Ticket findTicketByTicketCode(String ticketCode);
+    Optional<Ticket> findTicketByTicketCode(String ticketCode);
 }
