33c81b8d9c29758721de699f2d79928fbb6d1ae9
Fredrick Maina <fchege04@gmail.com>
2025-01-19 22:28:03 +0300

    Add verify account method

--- src/main/java/com/fredmaina/event_management/AuthService/controllers/AuthController.java
+++ src/main/java/com/fredmaina/event_management/AuthService/controllers/AuthController.java
@@ -4,12 +4,15 @@ import com.fredmaina.event_management.AuthService.DTOs.AuthResponse;
 import com.fredmaina.event_management.AuthService.DTOs.RegisterRequest;
 import com.fredmaina.event_management.AuthService.DTOs.LoginRequest;
 import com.fredmaina.event_management.AuthService.services.AuthService;
+import org.apache.tomcat.util.http.parser.Authorization;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import software.amazon.awssdk.services.kms.model.VerifyRequest;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = "*") // Optional: Allow requests from all origins
+@CrossOrigin(origins = "http://localhost:5173", allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.DELETE, RequestMethod.PUT, RequestMethod.PATCH, RequestMethod.OPTIONS})
+
 public class AuthController {
 
     @Autowired
@@ -35,5 +38,11 @@ public class AuthController {
     @PostMapping("/login")
     public AuthResponse login(@RequestBody LoginRequest loginRequest) {
         return authService.authenticateUser(loginRequest);
+    }
+    @PostMapping("/verify")
+    public AuthResponse verify(@RequestBody int code, @RequestHeader("Authorization") String token) {
+        token = token.replace("Bearer ", "").trim();
+        return authService.verifyUser(code,token);
+
     }
 }
