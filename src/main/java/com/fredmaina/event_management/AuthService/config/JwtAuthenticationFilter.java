cbbc7ca7f8d5c0a5481976f21e2a557d8b165252
Fredrick Maina <fchege04@gmail.com>
2025-01-19 22:32:27 +0300

    Ensure an account is verified before lettingn them into the filterchain

--- src/main/java/com/fredmaina/event_management/AuthService/config/JwtAuthenticationFilter.java
+++ src/main/java/com/fredmaina/event_management/AuthService/config/JwtAuthenticationFilter.java
@@ -39,7 +39,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             UserDetails userDetails = userDetailsService.loadUserByUsername(username);
 
-            if (jwtService.isTokenValid(jwt)) {
+
+            if (jwtService.isTokenValid(jwt)&& userDetails.isEnabled()) {
                 UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
                         userDetails, null, userDetails.getAuthorities());
                 authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
