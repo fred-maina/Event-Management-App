53fee4ed38f70c06bcf33923bdddec889708675e
Fredrick Maina <fchege04@gmail.com>
2025-01-20 12:57:07 +0300

    Add a send HTML email method

--- src/main/java/com/fredmaina/event_management/Email/Service/EmailService.java
+++ src/main/java/com/fredmaina/event_management/Email/Service/EmailService.java
@@ -1,14 +1,25 @@
 package com.fredmaina.event_management.Email.Service;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
 
 @Service
 public class EmailService {
     @Autowired
     JavaMailSender javaMailSender;
+    @Autowired
+    TemplateEngine templateEngine;
+
+    @Async
     public void sendEmail(String to, String subject, String body) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(to);
@@ -16,4 +27,27 @@ public class EmailService {
         message.setText(body);
         javaMailSender.send(message);
     }
+    @Async
+    public void sendHtmlEmail(String to, int verificationCode,String fullName) {
+        Context context = new Context();
+        context.setVariable("verificationCode", verificationCode);
+        context.setVariable("fullName", fullName);
+
+        String htmlContent = templateEngine.process("email-verification", context);
+
+        MimeMessage message = javaMailSender.createMimeMessage();
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setTo(to);
+            helper.setSubject("Account Verification");
+            helper.setText(htmlContent, true);
+            javaMailSender.send(message);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+            // Handle exception
+
+        }
+    }
+
+
 }
