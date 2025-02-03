43a4bedddfe33bc4753fed88e59de8409d1fe82c
Fredrick Maina <fchege04@gmail.com>
2025-02-03 22:38:09 +0300

    Fix Paging Warning

--- src/main/java/com/fredmaina/event_management/EventManagementApplication.java
+++ src/main/java/com/fredmaina/event_management/EventManagementApplication.java
@@ -4,10 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.scheduling.annotation.EnableAsync;
+
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @EnableAsync
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class EventManagementApplication {
 
 	public static void main(String[] args) {
