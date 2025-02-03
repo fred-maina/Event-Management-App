f70cbac66e770fecab0308885bb98ed63c065f0e
Fredrick Maina <fchege04@gmail.com>
2025-02-03 22:37:48 +0300

    Add event Type to Event DTO

--- src/main/java/com/fredmaina/event_management/EventCreationService/DTOs/EventDto.java
+++ src/main/java/com/fredmaina/event_management/EventCreationService/DTOs/EventDto.java
@@ -1,9 +1,13 @@
 package com.fredmaina.event_management.EventCreationService.DTOs;
 
+import com.fasterxml.jackson.annotation.*;
+import com.fredmaina.event_management.EventCreationService.Models.EventType;
 import lombok.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.UUID;
 
 @Getter
@@ -12,6 +16,18 @@ import java.util.UUID;
 @ToString
 @NoArgsConstructor
 public class EventDto  {
+    public EventDto(UUID id, String eventName, LocalDateTime eventStartDate, LocalDateTime eventEndDate, String eventVenue, Integer eventCapacity, UUID creatorId, String posterUrl, List<TicketTypeDTO> ticketType, Set<EventType> eventTypes) {
+        this.id = id;
+        this.eventName = eventName;
+        this.eventStartDate = eventStartDate;
+        this.eventEndDate = eventEndDate;
+        this.eventVenue = eventVenue;
+        this.eventCapacity = eventCapacity;
+        this.creatorId = creatorId;
+        this.posterUrl = posterUrl;
+        this.ticketType = ticketType;
+        this.eventTypes = eventTypes;
+    }
 
     private UUID id;
     private  String eventName;
@@ -22,5 +38,8 @@ public class EventDto  {
     private UUID creatorId;
     private String posterUrl;
     private List<TicketTypeDTO> ticketType;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private Set<Long> eventTypeIds;
 
+    private Set<EventType> eventTypes;
 }
