f866bcdd8d6662fef939a6c8c0d1e6563dda4e12
Fredrick Maina <fchege04@gmail.com>
2025-02-08 15:32:11 +0300

    Add to string method

--- src/main/java/com/fredmaina/event_management/AuthService/DTOs/RegisterRequest.java
+++ src/main/java/com/fredmaina/event_management/AuthService/DTOs/RegisterRequest.java
@@ -4,11 +4,12 @@ package com.fredmaina.event_management.AuthService.DTOs;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
 @AllArgsConstructor
-
+@ToString
 public class RegisterRequest {
     private String firstName;
     private String lastName ;
