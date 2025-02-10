78baafd08f89219c408d530c85e7ae0d8130133d
Fredrick Maina <fchege04@gmail.com>
2025-02-10 17:37:12 +0300

    Refactor Lambda function and remove hardcoded environment variables

--- src/main/java/com/fredmaina/event_management/AWS/services/LambdaService.java
+++ src/main/java/com/fredmaina/event_management/AWS/services/LambdaService.java
@@ -8,28 +8,16 @@ import software.amazon.awssdk.core.SdkBytes;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
 import java.util.Map;
 
 @Service
 public class LambdaService {
     @Autowired
-    LambdaClient lambdaClient;
+    LambdaClient lambdaClient;  // Use the autowired LambdaClient
 
     private final ObjectMapper objectMapper = new ObjectMapper();  // Jackson's ObjectMapper for JSON conversion
 
-    public String invokeLambda() {
-        // Create a Map for payload data
-        Map<String, Object> payloadMap = new HashMap<>();
-        payloadMap.put("event_name", "Sample Event");
-        payloadMap.put("event_date", "2025-01-01");
-        payloadMap.put("buyer_name", "John Doe");
-        payloadMap.put("ticket_code", "12345");
-        payloadMap.put("payment_mode", "Credit Card");
-        payloadMap.put("ticket_type", "VIP");
-        payloadMap.put("ticket_price", "100");
-        payloadMap.put("purchase_time", "2025-01-01T10:00:00Z");
-
+    public String invokeLambda(Map<String, Object> payloadMap) {
         // Convert the Map to a JSON string
         String jsonString = convertMapToJsonString(payloadMap);
 
@@ -37,9 +25,7 @@ public class LambdaService {
         SdkBytes sdkBytes = SdkBytes.fromByteArray(jsonString.getBytes(StandardCharsets.UTF_8));
 
         // Invoke Lambda
-        String response = callLambdaFunction(sdkBytes);
-
-        return response;
+        return callLambdaFunction(sdkBytes);
     }
 
     private String convertMapToJsonString(Map<String, Object> map) {
@@ -55,13 +41,13 @@ public class LambdaService {
     private String callLambdaFunction(SdkBytes payload) {
         // Build the invoke request
         InvokeRequest invokeRequest = InvokeRequest.builder()
-                .functionName("QRCodeGenerator")  // Replace with your Lambda function name
+                .functionName("arn:aws:lambda:eu-north-1:465921435493:function:GenerateUserTicket")  // Your Lambda function ARN
                 .payload(payload)
                 .build();
 
-        // Call Lambda (you need to use the LambdaClient here)
-        try (LambdaClient lambdaClient = LambdaClient.create()) {
-            InvokeResponse invokeResponse = lambdaClient.invoke(invokeRequest);
+        // Call Lambda (use the autowired LambdaClient)
+        try {
+            InvokeResponse invokeResponse = lambdaClient.invoke(invokeRequest);  // Use the autowired lambdaClient
             // Get the response as string
             return invokeResponse.payload().asString(StandardCharsets.UTF_8);
         } catch (Exception e) {
